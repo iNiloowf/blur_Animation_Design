3394e4015c6509454d47073afc89ca53507468c2
iNiloowf <74246387+iNiloowf@users.noreply.github.com>
2023-02-10 14:58:44 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+const headers = document.querySelector('header');
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookied for improve functionality and analystics.<button class="btn btn--close-cookie"> Got it!</button>';
+headers.append(message);
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+/*
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1 + min));
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector(`.nav__link`).addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  this.textContent = 'NOT AGAIN';
+});
+document.querySelector(`.nav__links`).addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+document.querySelector(`.header`).addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+*/
